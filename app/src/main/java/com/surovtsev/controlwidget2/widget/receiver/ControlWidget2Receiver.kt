b16948a272396e8ce3e615f586f396f282d61c56
surovtsev-alexander <alexander.surovtsev.ya@yandex.ru>
2022-07-06 00:25:05 +0300

    turn on/off wifi

--- app/src/main/java/com/surovtsev/controlwidget2/widget/receiver/ControlWidget2Receiver.kt
+++ app/src/main/java/com/surovtsev/controlwidget2/widget/receiver/ControlWidget2Receiver.kt
@@ -1,8 +1,11 @@
 package com.surovtsev.controlwidget2.widget.receiver
 
 import android.appwidget.AppWidgetManager
+import android.bluetooth.BluetoothAdapter
 import android.content.Context
 import android.content.Intent
+import android.location.LocationManager
+import android.net.wifi.WifiManager
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.glance.appwidget.GlanceAppWidget
@@ -14,6 +17,7 @@ import com.surovtsev.controlwidget2.controlsinfobriadcastreceiver.ControlsInfoBr
 import com.surovtsev.controlwidget2.features.controlwidget2.domain.repository.ControlsInformationRepo
 import com.surovtsev.controlwidget2.features.controlwidget2.domain.usecase.ControlsInformationUseCase
 import com.surovtsev.controlwidget2.widget.ControlWidget2
+import com.surovtsev.controlwidget2.widget.callback.CommandToControlWidget2Action
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.MainScope
@@ -39,6 +43,15 @@ class ControlWidget2Receiver: GlanceAppWidgetReceiver() {
 
     @Inject
     lateinit var controlsInformationRepo: ControlsInformationRepo
+
+    @Inject
+    lateinit var wifiManager: WifiManager
+
+    @Inject
+    lateinit var bluetoothAdapter: BluetoothAdapter
+
+    @Inject
+    lateinit var locationManager: LocationManager
 
     private var updateJob: Job? = null
 
@@ -97,6 +110,48 @@ class ControlWidget2Receiver: GlanceAppWidgetReceiver() {
         super.onReceive(context, intent)
 
         logcat { "onReceive; intent: $intent" }
+
+        if (intent.action == CommandToControlWidget2Action.commandToControlWidget2) {
+            val bundle = intent.extras
+
+            bundle?.keySet()?.map { commandName ->
+                val enable = bundle[commandName]?.run { this as Boolean }
+                if (enable != null) {
+                    when(commandName) {
+                        wifiState.key.name -> {
+                            wifiManager.isWifiEnabled = enable
+                        }
+//                        bluetoothState.key.name -> {
+//                            if (ActivityCompat.checkSelfPermission(context,
+//                                    Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED
+//                            ) {
+//                                // TODO: Consider calling
+//                                //    ActivityCompat#requestPermissions
+//                                // here to request the missing permissions, and then overriding
+//                                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                                //                                          int[] grantResults)
+//                                // to handle the case where the user grants the permission. See the documentation
+//                                // for ActivityCompat#requestPermissions for more details.
+//                                return
+//                            }
+//                            if (enable) {
+//                                bluetoothAdapter.enable()
+//                            } else {
+//                                bluetoothAdapter.disable()
+//                            }
+//                        }
+//                        gpsState.key.name -> {
+//                            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+//                            if (enable) {
+//                                context.startService(intent)
+//                            } else {
+//                                context.stopService(intent)
+//                            }
+//                        }
+                    }
+                }
+            }
+        }
     }
 
     companion object {
