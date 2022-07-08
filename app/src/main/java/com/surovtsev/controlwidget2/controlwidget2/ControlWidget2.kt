cf2f1690fb37676c5a336fab0c53f782b22132a4
surovtsev-alexander <alexander.surovtsev.ya@yandex.ru>
2022-07-08 05:10:28 +0300

    remove useless code

--- app/src/main/java/com/surovtsev/controlwidget2/controlwidget2/ControlWidget2.kt
+++ app/src/main/java/com/surovtsev/controlwidget2/controlwidget2/ControlWidget2.kt
@@ -16,12 +16,7 @@ class ControlWidget2: GlanceAppWidget() {
         ControlWidget2Content(
             modifier = GlanceModifier
                 .fillMaxSize()
-//                .background(
-//                    day = Color.Cyan,
-//                    night = Color.DarkGray,
-//                )
                 .appWidgetBackground()
-//                .cornerRadius(16.dp)
                 .padding(8.dp),
         )
     }
